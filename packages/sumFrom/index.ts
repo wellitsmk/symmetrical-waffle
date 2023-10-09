02de9119165120fac2e25c35d6d7e106262ad8be
Mohit Kumar <szmohitkumar@gmail.com>
2023-10-10 02:32:17 +0530

    feat: sumOf added

--- packages/sumFrom/index.ts
+++ packages/sumFrom/index.ts
@@ -2,3 +2,8 @@
 export const sumFrom = (x: number, y: number) : number => {
     return x === y ? x : x + sumFrom(++x, y)
 }
+
+export const sumOf = (x: number, y: number) : number => {
+    return x + y
+}
+
